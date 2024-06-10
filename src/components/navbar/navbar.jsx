94848d47e753393516fedf11557ecbb4baac5f91
Minura Dinupriya <mranaweera793@gmail.com>
2024-06-10 15:34:07 +0530

    Add navlinks and some css

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,10 +1,22 @@
-import "./navbar.scss"
+import "./navbar.scss";
 
 function Navbar() {
   return (
     <nav>
-      <div className="">left</div>
-      <div className="">right</div>
+      <div className="left">
+        <a href="" className="logo">
+          <img src="/logo.png" alt=""></img>
+          <span>DinEstate</span>
+        </a>
+        <a href="">Home</a>
+        <a href="">About</a>
+        <a href="">Contact</a>
+        <a href="">Agents</a>
+      </div>
+      <div className="right">
+        <a href="">Sign in</a>
+        <a href="" className="register">Sign up</a>
+      </div>
     </nav>
   );
 }
