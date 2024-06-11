2fc138c0fe6d8ae3850cfedeb8ce8527d0863a76
Minura Dinupriya <mranaweera793@gmail.com>
2024-06-11 18:47:28 +0530

    Create home page structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import Navbar from './components/navbar/navbar'
-import './layout.scss'
+import Navbar from "./components/navbar/navbar";
+import "./layout.scss";
+import HomePage from "./pages/homePage/homePage";
 
 function App() {
   return (
-    <div className="layout"><Navbar>asdf</Navbar></div>
-  )
+    <div className="layout">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="content">
+        <HomePage />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/homePage/homePage.jsx
@@ -0,0 +1,9 @@
+import "./homePage.scss";
+
+function HomePage(){
+    return(
+        <div>Home</div>
+    )
+}
+
+export default HomePage;
