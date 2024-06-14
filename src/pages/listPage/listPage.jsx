c212f97a4af4f4e72cb3200553c44e1ab3a1c34c
Minura Dinupriya <mranaweera793@gmail.com>
2024-06-14 12:14:41 +0530

    Create structure for listPage

--- src/pages/listPage/listPage.jsx
+++ src/pages/listPage/listPage.jsx
@@ -1,8 +1,22 @@
+import Filter from "../../components/filter/filter";
+import Card from "../../components/card/card";
+import { listData } from "../../lib/dummyData";
 import "./listPage.scss";
 
 function ListPage(){
+    const data=listData;
     return(
-        <div className="listPage">ListPage</div>
+        <div className="listPage">
+            <div className="listContainer">
+                <div className="wrapper">
+                    <Filter/>
+                    {data.map(item=>(
+                        <Card key={item.id} item={item}/>
+                    ))}
+                </div>
+            </div>
+            <div className="mapContainer">Map</div>
+        </div>
     )
 }
 
