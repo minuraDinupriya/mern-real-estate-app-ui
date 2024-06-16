761bf03842856753c1c539dba3bfc26206c79250
Minura Dinupriya <mranaweera793@gmail.com>
2024-06-16 21:00:38 +0530

    Create html structure for the card

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,9 +1,45 @@
+import { Link } from "react-router-dom";
 import "./card.scss";
 
-function Card(){
-    return(
-        <div className="card">Card</div>
-    )
+function Card({ item }) {
+  return (
+    <div className="card">
+      <Link to={`/${item.id}`} className="imageContainer">
+      <img src={item.img} alt="" />
+      </Link>
+      <div className="textContainer">
+        <h2 className="title">
+            <Link to={`/${item.id}`}>{item.title}</Link>
+        </h2>
+        <p className="address">
+            <img src="/pin.png"></img>
+            <span>{item.address}</span>
+        </p>
+        <p className="price">${item.price}</p>
+
+        <div className="bottom">
+            <div className="features">
+                <div className="feature">
+                    <img src="/bed.png"></img>
+                    <span>{item.bedroom}</span>
+                </div>
+                <div className="feature">
+                    <img src="/bathroom.png"></img>
+                    <span>{item.bathroom}</span>
+                </div>
+            </div>
+            <div className="icons">
+                <div className="icon">
+                    <img src="/save.png" alt="" />
+                </div>
+                <div className="icon">
+                    <img src="/chat.png" alt="" />
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
