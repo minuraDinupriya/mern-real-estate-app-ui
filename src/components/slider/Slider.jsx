24701fde608ca87db9849a51417222f4447677ee
Minura Dinupriya <mranaweera793@gmail.com>
2024-06-20 07:04:28 +0530

    Add slider static images

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,8 +1,22 @@
 import "./slider.scss";
 
-function Slider(){
+function Slider({images}){
     return(
-        <div>Slider</div>
+        <div className="slider">
+            <div className="bigImage">
+                <img src={images[0]} alt="" />
+            </div>
+
+            <div className="smallImages">
+            {images.slice(1).map((image, index) => (
+          <img
+            src={image}
+            alt=""
+            key={index}
+          />
+        ))}
+            </div>
+        </div>
     )
 }
 
