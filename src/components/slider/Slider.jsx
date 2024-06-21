82490c6208cbe0ad40e28ff1b6cadc94d6878fc7
Minura Dinupriya <mranaweera793@gmail.com>
2024-06-21 07:10:40 +0530

    Create static full slider

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,23 +1,35 @@
 import "./slider.scss";
 
-function Slider({images}){
-    return(
-        <div className="slider">
-            <div className="bigImage">
-                <img src={images[0]} alt="" />
-            </div>
-
-            <div className="smallImages">
-            {images.slice(1).map((image, index) => (
-          <img
-            src={image}
-            alt=""
-            key={index}
-          />
-        ))}
-            </div>
+function Slider({ images }) {
+  return (
+    <div className="slider">
+      <div className="fullSlider">
+        <div className="arrow">
+          <img src="/arrow.png" alt="" />
         </div>
-    )
+
+        <div className="imageContainer">
+          <img src={images[0]} alt="" />
+        </div>
+
+        <div className="arrow">
+          <img src="/arrow.png" className="right" alt="" />
+        </div>
+
+        <div className="close">X</div>
+      </div>
+
+      <div className="bigImage">
+        <img src={images[0]} alt="" />
+      </div>
+
+      <div className="smallImages">
+        {images.slice(1).map((image, index) => (
+          <img src={image} alt="" key={index} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Slider;
