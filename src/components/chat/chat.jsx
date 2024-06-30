8d81c59eac7e63844d8361f932b6ba46bc717ae1
Minura Dinupriya <mranaweera793@gmail.com>
2024-06-30 17:14:34 +0530

    Create html structure of chat

---
+++ src/components/chat/chat.jsx
@@ -0,0 +1,144 @@
+import "./chat.scss";
+
+function Chat() {
+  return (
+    <div className="chat">
+      <div className="messeges">
+        <h1>Messeges</h1>
+
+        <div className="messege">
+          <img
+            src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+          />
+          <span>Minura Dinupriya</span>
+          <p>Lorem ipsum dolor sit amet...</p>
+        </div>
+
+        <div className="messege">
+          <img
+            src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+          />
+          <span>Minura Dinupriya</span>
+          <p>Lorem ipsum dolor sit amet...</p>
+        </div>
+
+        <div className="messege">
+          <img
+            src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+          />
+          <span>Minura Dinupriya</span>
+          <p>Lorem ipsum dolor sit amet...</p>
+        </div>
+
+        <div className="messege">
+          <img
+            src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+          />
+          <span>Minura Dinupriya</span>
+          <p>Lorem ipsum dolor sit amet...</p>
+        </div>
+
+        <div className="messege">
+          <img
+            src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+          />
+          <span>Minura Dinupriya</span>
+          <p>Lorem ipsum dolor sit amet...</p>
+        </div>
+
+        <div className="messege">
+          <img
+            src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+          />
+          <span>Minura Dinupriya</span>
+          <p>Lorem ipsum dolor sit amet...</p>
+        </div>
+      </div>
+
+      <div className="chatBox">
+        <div className="top">
+          <div className="user">
+            <img
+              src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+              alt=""
+            />
+            Minura Dinupriya
+          </div>
+          <span>X</span>
+        </div>
+
+
+        <div className="center">
+
+            <div className="chatMessege own">
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+                <span>1 hour ago</span>
+            </div>
+
+            <div className="chatMessege">
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+                <span>1 hour ago</span>
+            </div>
+
+            <div className="chatMessege own">
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+                <span>1 hour ago</span>
+            </div>
+
+            <div className="chatMessege">
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+                <span>1 hour ago</span>
+            </div>
+
+            <div className="chatMessege own">
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+                <span>1 hour ago</span>
+            </div>
+
+            <div className="chatMessege">
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+                <span>1 hour ago</span>
+            </div>
+
+            <div className="chatMessege own">
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+                <span>1 hour ago</span>
+            </div>
+
+            <div className="chatMessege">
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+                <span>1 hour ago</span>
+            </div>
+
+            <div className="chatMessege own">
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+                <span>1 hour ago</span>
+            </div>
+
+            <div className="chatMessege">
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+                <span>1 hour ago</span>
+            </div>
+
+            <div className="chatMessege own">
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+                <span>1 hour ago</span>
+            </div>
+
+        </div>
+        <div className="bottom">
+            <textarea name="" id=""></textarea>
+            <button>Send</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Chat;
