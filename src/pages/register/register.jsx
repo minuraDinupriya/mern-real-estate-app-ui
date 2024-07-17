cb6745a98b8ded1cb13df3d5a574fef7074e11b1
Minura Dinupriya <mranaweera793@gmail.com>
2024-07-17 14:45:07 +0530

    Add user registration functionality

--- src/pages/register/register.jsx
+++ src/pages/register/register.jsx
@@ -2,14 +2,18 @@ import "./register.scss";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useState } from "react";
+import apiRequest from "../../lib/apiRequest";
 
 function Register() {
   const [error, setError] = useState("");
-
+  const [isLoading, setIsLoading] = useState(false);
+  
   const navigate = useNavigate();
-
+  
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setIsLoading(true);
+    setError("")
 
     const formData = new FormData(e.target);
 
@@ -18,15 +22,18 @@ function Register() {
     const password = formData.get("password");
 
     try {
-      const res = await axios.post("http://localhost:8800/api/auth/register", {
+      const res = await apiRequest.post("/auth/register", {
         username,
         email,
         password,
       });
+      // console.log(res);
       navigate("/login");
     } catch (err) {
       console.log(err);
       setError(err.response.data.message);
+    }finally{
+      setIsLoading(false);
     }
   };
   return (
@@ -37,7 +44,7 @@ function Register() {
           <input name="username" type="text" placeholder="Username" />
           <input name="email" type="text" placeholder="Email" />
           <input name="password" type="password" placeholder="Password" />
-          <button>Register</button>
+          <button disabled={isLoading}>Register</button>
           {error && <span>{error}</span>}
           <Link to="/login">Do you have an account?</Link>
         </form>
