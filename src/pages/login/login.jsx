698116bd67f63284f7e7b2da6d004ad28942e215
Minura Dinupriya <mranaweera793@gmail.com>
2024-07-31 19:30:19 +0530

    Minor update

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,18 +1,21 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import "./login.scss";
 import { Link, useNavigate } from "react-router-dom";
 import apiRequest from "../../lib/apiRequest";
+import { AuthContext } from "../../context/AuthContext";
 
 function Login() {
   const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+
+  const {updateUser} = useContext(AuthContext)
+
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setIsLoading(true);
     setError("");
-
     const formData = new FormData(e.target);
 
     const username = formData.get("username");
@@ -24,19 +27,14 @@ function Login() {
         password,
       });
 
-      console.log(res);
+      updateUser(res.data)
 
-      localStorage.setItem("user", JSON.stringify(res.data))
       navigate("/");
-
     } catch (err) {
-      console.log(err);
       setError(err.response.data.message);
-      
-    }finally{
+    } finally {
       setIsLoading(false);
     }
-    
   };
   return (
     <div className="login">
@@ -57,11 +55,9 @@ function Login() {
             required
             placeholder="Password"
           />
-          <button  disabled={isLoading}>Login</button>
+          <button disabled={isLoading}>Login</button>
           {error && <span>{error}</span>}
-          <Link to="/register">
-            {"Don't"} you have an account?
-          </Link>
+          <Link to="/register">{"Don't"} you have an account?</Link>
         </form>
       </div>
       <div className="imgContainer">
