8f30f8fe65824544de5dff22e17ff214710c752f
Minura Dinupriya <mranaweera793@gmail.com>
2024-08-19 19:36:57 +0530

    Create profile update page

---
+++ src/pages/profileUpdatePage/profileUpdatePage.jsx
@@ -0,0 +1,9 @@
+import './profileUpdatePage.scss';
+ 
+function ProfileUpdatePage(){
+    return(
+        <div className="profileUpdatePage">Profile Update Page</div>
+    )
+}
+
+export default ProfileUpdatePage;
