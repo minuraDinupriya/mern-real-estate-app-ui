097df0a0574fd4cb3275eddc2e54ebf39ca61d3b
Minura Dinupriya <mranaweera793@gmail.com>
2024-09-05 08:53:26 +0530

    Create single page loader

---
+++ src/lib/loaders.js
@@ -0,0 +1,7 @@
+import { defer } from "react-router-dom";
+import apiRequest from "./apiRequest";
+
+export const singlePageLoader = async ({ request, params }) => {
+  const res = await apiRequest("/posts/" + params.id);
+  return res.data;
+};
