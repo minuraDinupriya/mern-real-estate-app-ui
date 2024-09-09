fb0730c0f6b64704b9718232e3393f697913f3f2
Minura Dinupriya <mranaweera793@gmail.com>
2024-09-09 15:03:56 +0530

    Create list page loader

--- src/lib/loaders.js
+++ src/lib/loaders.js
@@ -5,3 +5,11 @@ export const singlePageLoader = async ({ request, params }) => {
   const res = await apiRequest("/posts/" + params.id);
   return res.data;
 };
+
+export const listPageLoader = async ({ request, params }) => {
+  const query = request.url.split("?")[1];
+  const postPromise = apiRequest("/posts?" + query);
+  return defer({
+    postResponse: postPromise,
+  });
+};
