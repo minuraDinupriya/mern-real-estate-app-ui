91a6a898000a47920e5029a6ef94a3089bca7ef7
Minura Dinupriya <mranaweera793@gmail.com>
2024-09-14 20:38:40 +0530

    Photo list added

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -5,26 +5,25 @@ function Card({ item }) {
   return (
     <div className="card">
       <Link to={`/${item.id}`} className="imageContainer">
-        <img src={item.img} alt="" />
+        <img src={item.images[0]} alt="" />
       </Link>
       <div className="textContainer">
         <h2 className="title">
           <Link to={`/${item.id}`}>{item.title}</Link>
         </h2>
         <p className="address">
-          <img src="/pin.png"></img>
+          <img src="/pin.png" alt="" />
           <span>{item.address}</span>
         </p>
-        <p className="price">${item.price}</p>
-
+        <p className="price">$ {item.price}</p>
         <div className="bottom">
           <div className="features">
             <div className="feature">
-              <img src="/bed.png"></img>
+              <img src="/bed.png" alt="" />
               <span>{item.bedroom} bedroom</span>
             </div>
             <div className="feature">
-              <img src="/bath.png"></img>
+              <img src="/bath.png" alt="" />
               <span>{item.bathroom} bathroom</span>
             </div>
           </div>
