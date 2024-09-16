ee6c142273da2dae1875eac6d0dd0dff83aa13f8
Minura Dinupriya <mranaweera793@gmail.com>
2024-09-16 13:19:09 +0530

    Add navigation to searched item

--- src/components/searchBar/searchBar.jsx
+++ src/components/searchBar/searchBar.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import "./searchBar.scss";
+import { Link } from "react-router-dom";
 
-const type = ["buy", "rent"];
+const types = ["buy", "rent"];
 
 function SearchBar() {
   const [query, setQuery] = useState({
     type: "buy",
-    location: "",
+    city: "",
     minPrice: 0,
     maxPrice: 0,
   });
@@ -15,10 +16,14 @@ function SearchBar() {
     setQuery((prev) => ({ ...prev, type: val }));
   };
 
+  const handleChange = (e) => {
+    setQuery((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
   return (
     <div className="searchBar">
       <div className="type">
-        {type.map((type) => (
+        {types.map((type) => (
           <button
             key={type}
             onClick={() => switchType(type)}
@@ -29,24 +34,35 @@ function SearchBar() {
         ))}
       </div>
       <form>
-        <input type="text" name="location" placeholder="City Location"></input>
+        <input
+          type="text"
+          name="city"
+          placeholder="City"
+          onChange={handleChange}
+        />
         <input
           type="number"
           name="minPrice"
           min={0}
           max={10000000}
           placeholder="Min Price"
-        ></input>
+          onChange={handleChange}
+        />
         <input
           type="number"
           name="maxPrice"
           min={0}
           max={10000000}
-          placeholder="Max price"
-        ></input>
-        <button>
-          <img src="/search.png"></img>
-        </button>
+          placeholder="Max Price"
+          onChange={handleChange}
+        />
+        <Link
+          to={`/list?type=${query.type}&city=${query.city}&minPrice=${query.minPrice}&maxPrice=${query.maxPrice}`}
+        >
+          <button>
+            <img src="/search.png" alt="" />
+          </button>
+        </Link>
       </form>
     </div>
   );
